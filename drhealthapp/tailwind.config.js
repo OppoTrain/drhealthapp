d73e5d600c62940a417a8046584290037865505b
Hp <abuasloudt@gmail.com>
2025-03-24 21:33:49 +0200

    adming login page

---
+++ drhealthapp/tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/react/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
